035210e46b086b5e101a6055aedb924eb45431a1
EasonLin0716 <uu9924079@gmail.com>
2022-10-06 21:49:34 +0800

    update log

--- server.js
+++ server.js
@@ -58,4 +58,4 @@ app.get("*", (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+app.listen(port, () => console.log(`app listening on port ${port}`));
